84f9b4a703ddacd65e738c6e21033e57acea434c
Hemant Sanvaria <hsanvaria@ccistudios.com>
2014-11-20 09:05:09 -0500

    Final changes

--- form.php
+++ form.php
@@ -17,7 +17,7 @@ $data 			= array(); 		// array to pass back data
 
 	
 	$to = "lbujaki@ccistudios.com";
-	$subject="contact info";
+	$subject="Website Inquiry";
 	$name = $_POST['name'];
 	$phone = $_POST['phone'];
 	$msg=  $_POST['msg'];
@@ -28,14 +28,14 @@ $data 			= array(); 		// array to pass back data
 
 	$headers = "MIME-Version: 1.0" . "\r\n";
 	$headers .= " X-Priority: 2\nX-MSmail-Priority: high" . "\r\n";
-	$headers .= "From: CCI Studios<info@uniquewebdesigncanada.com>" . "\r\n" .
+	$headers .= "From: UWD<info@uniquewebdesigncanada.com>" . "\r\n" .
 	"Reply-To: lbujaki@ccistudios.com" . "\r\n" .
 	"X-Mailer: PHP/" . phpversion() ;
 	$message= 'Name:  '.$name ."\r\n" .'Email:  '.$email ."\r\n" .'Phone:  '.$phone ."\r\n" .'Message:  '.$msg ."\r\n" .'Stay connected:  '.$check ;
 	mail($to,$subject,$message,$headers);
 
-		$data['success'] = true;
-		$data['message'] = 'Thank You';
+		$data['success'] = true; 
+		$data['message'] = "Thank You<br>We appreciate your inquiry and will be in touch shortly!";
 	}
 
 	echo json_encode($data);	
